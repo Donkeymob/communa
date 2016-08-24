fa7f5f3ca645a9b7f601bfdefebf2c406b7a14fd
zoro <zoro@donkeymob.com>
2016-08-24 18:06:17 -0400

    rough in js alternative to typewriter animation

--- ready/js/mighty.js
+++ ready/js/mighty.js
@@ -6,4 +6,13 @@ $(document).ready (function(){
                $("#success-alert").alert('close');
                 });
             });
- });
+
+  var type_text = $('#type_text');
+  // sync with css animation
+  // type_text.text('Give Opinion').delay(2000).text('Earn rewards');
+  type_text.text('give opinion');
+  var switch_text_timer = window.setTimeout( switch_text, 2000 );
+  function switch_text(){
+  	type_text.text('earn rewards')
+  }
+});
